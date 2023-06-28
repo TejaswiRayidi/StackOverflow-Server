a1be05c5432bbee0ccbc8a76683f895a4c30b071
TejaswiRayidi <201232@iiitt.ac.in>
2023-06-28 13:27:01 +0530

    first commit

---
+++ routes/users.js
@@ -0,0 +1,16 @@
+import express from "express";
+import auth from '../middlewares/auth.js'
+
+import { login, signup } from "../controllers/auth.js";
+import { getAllUsers, updateProfile } from '../controllers/users.js'
+
+
+const router = express.Router();
+
+router.post("/signup", signup);
+router.post("/login", login);
+router.get('/getAllUsers',getAllUsers)
+router.patch('/update/:id', auth, updateProfile)
+
+
+export default router;
